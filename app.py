16bfc60e4353c1e420b87769f98fa832f17c6afd
SCiarella <simoneciarella@gmail.com>
2024-10-31 18:20:45 +0100

    attempt to display videos

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title="My Page", header="Welcome", message="This is my webpage!")
+    return render_template('index.html', title="DeepDip", header="DeepDip", message="Example")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/videos')
+def videos():
+    return render_template('videos.html')
 
 
 if __name__ == '__main__':
